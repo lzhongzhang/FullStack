4f96d933d4638696bba32bc90648df1353e7d5a5
Joeelamor <lizhongzhang330@gmail.com>
2019-05-02 11:13:30 -0700

    Change some logics

--- react/1_iphonescreen/src/App.js
+++ react/1_iphonescreen/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+          <div className="header">Header
+          </div>
+          <div className="content">
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+
+            <div className = "box"></div>
+            <div className = "box"></div>
+          </div>
+          <div className="footer">
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+            <div className = "box"></div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
