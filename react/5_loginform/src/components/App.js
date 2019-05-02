ee14fd5aa360bdd5e0143e8d5d6423464e1c57cd
Joeelamor <lizhongzhang330@gmail.com>
2019-05-02 14:30:55 -0700

    Add new practice 9

---
+++ react/5_loginform/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+
+    );
+  }
+}
+export default App;
